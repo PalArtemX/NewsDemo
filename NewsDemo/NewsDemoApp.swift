091793267da1009abe6ae8088490c27cd8129b11
Artem Paliutin <pal.artem@icloud.com>
2022-06-24 14:17:44 +0300

    Initial Commit

---
+++ NewsDemo/NewsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsDemoApp.swift
+//  NewsDemo
+//
+//  Created by Artem Paliutin on 24/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct NewsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
