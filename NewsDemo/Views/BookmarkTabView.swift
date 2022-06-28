66d4d8b3719c55b3feda47b48b14515ccceb0b8b
Artem Paliutin <pal.artem@icloud.com>
2022-06-28 13:41:51 +0300

    added BookmarkTabView

---
+++ NewsDemo/Views/BookmarkTabView.swift
@@ -0,0 +1,51 @@
+//
+//  BookmarkTabView.swift
+//  NewsDemo
+//
+//  Created by Artem Paliutin on 28/06/2022.
+//
+
+import SwiftUI
+
+struct BookmarkTabView: View {
+    @EnvironmentObject var articleBookmarkVM: ArticleBookmarkVM
+    
+    var body: some View {
+        NavigationStack {
+            ArticleListView(articles: articleBookmarkVM.bookmarks)
+                .overlay {
+                    overlayView(isEmpty: articleBookmarkVM.bookmarks.isEmpty)
+                }
+            
+                .navigationTitle("Saved Articles")
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+struct BookmarkTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        BookmarkTabView()
+            .environmentObject(ArticleBookmarkVM())
+    }
+}
+
+
+
+// MARK: Extension BookmarkTabView
+extension BookmarkTabView {
+    @ViewBuilder
+    private func overlayView(isEmpty: Bool) -> some View {
+        if isEmpty {
+            EmptyPlaceholderView(text: "No Saved Articles", image: Image(systemName: "bookmark"))
+        }
+    }
+}
--- NewsDemo/Views/MainView.swift
+++ NewsDemo/Views/MainView.swift
@@ -10,9 +10,16 @@ import SwiftUI
 struct MainView: View {
     var body: some View {
         TabView {
+            // MARK: - NewsTabView
             NewsTabView()
                 .tabItem {
                     Label("News", systemImage: "newspaper")
+                }
+            
+            // MARK: - BookmarkTabView
+            BookmarkTabView()
+                .tabItem {
+                    Label("Saved", systemImage: "bookmark")
                 }
         }
     }
@@ -31,5 +38,6 @@ struct MainView_Previews: PreviewProvider {
     static var previews: some View {
         MainView()
             .environmentObject(ArticleNewsVM())
+            .environmentObject(ArticleBookmarkVM())
     }
 }
