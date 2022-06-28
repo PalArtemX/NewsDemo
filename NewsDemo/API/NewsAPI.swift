ebe850de8657bef2d25c69ceabb69e2faaac9000
Artem Paliutin <pal.artem@icloud.com>
2022-06-28 16:43:01 +0300

    fix

--- NewsDemo/API/NewsAPI.swift
+++ NewsDemo/API/NewsAPI.swift
@@ -24,7 +24,41 @@ struct NewsAPI {
     
     
     func fetch(from category: Category) async throws -> [Article] {
-        let url = generateNewsURL(from: category)
+        try await fetchArticles(from: generateNewsURL(from: category))
+
+        
+    }
+    
+    func search(for query: String) async throws -> [Article] {
+        try await fetchArticles(from: generateSearchURL(from: query))
+    }
+    
+    
+    private func generateError(code: Int = 1, description: String) -> Error {
+        NSError(domain: "NewsAPI", code: code, userInfo: [NSLocalizedDescriptionKey: description])
+    }
+    
+    
+    private func generateNewsURL(from category: Category) -> URL {
+        var url = "https://newapi.org/v2/top-headlines?"
+        url += "apiKey=\(apiKey)"
+        url += "&language=en"
+        url += "&category=\(category.rawValue)"
+        return URL(string: url)!
+    }
+    
+    private func generateSearchURL(from query: String) -> URL {
+        let percentEncodedString = query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? query
+        
+        var url = "https://newapi.org/v2/everything?"
+        url += "apiKey=\(apiKey)"
+        url += "&language=en"
+        url += "&q=\(percentEncodedString)"
+        
+        return URL(string: url)!
+    }
+    
+    private func fetchArticles(from url: URL) async throws -> [Article] {
         let (data, response) = try await session.data(from: url)
         
         guard let response = response as? HTTPURLResponse else {
@@ -43,22 +77,5 @@ struct NewsAPI {
         default:
             throw generateError(description: "A server error occured")
         }
-        
-    }
-    
-    
-    
-    
-    private func generateError(code: Int = 1, description: String) -> Error {
-        NSError(domain: "NewsAPI", code: code, userInfo: [NSLocalizedDescriptionKey: description])
-    }
-    
-    
-    private func generateNewsURL(from category: Category) -> URL {
-        var url = "https://newapi.org/v2/top-headlines?"
-        url += "apiKey=\(apiKey)"
-        url += "&language=en"
-        url += "&category=\(category.rawValue)"
-        return URL(string: url)!
     }
 }
